0f638174ba6d6d45e20f6ec00ebf48c5622168b6
ramadbp <rdan60404@gmail.com>
2025-06-21 13:10:06 +0700

    import bootstrap dimain js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import CSS Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"; // Import CSS Bootstrap dimain
 import "bootstrap"; // (Optional: Import JS Bootstrap, jika butuh komponen interaktif)
 
 createApp(App).use(router).mount("#app");
